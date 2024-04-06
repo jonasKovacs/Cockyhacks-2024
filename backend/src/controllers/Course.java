5e78f0b91df3ef176258da68ca3b60dfe424da2a
jonasKovacs <jonas.kovacs.1@gmail.com>
2024-04-06 14:26:29 -0400

    added classes to controllers

---
+++ backend/src/controllers/Course.java
@@ -0,0 +1,11 @@
+import java.util.UUID;
+import java.util.ArrayList;
+
+public class Course {
+    public UUID id;
+    public String courseNumber;
+    public String title;
+    public String description;
+    public ArrayList<Review> reviews;
+
+}
