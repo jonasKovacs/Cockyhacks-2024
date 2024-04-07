c51f2d3f21d9d594eb9f016c1613e27e4f9ec131
Julia Grove <lauragrove@Julias-MBP-6.lan>
2024-04-07 11:11:43 -0400

    update frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState } from "react";
+import { Rating } from 'react-simple-star-rating'
 
 function App() {
 
@@ -8,6 +9,11 @@ function App() {
   const [selectedTitle, setSelectedTitle] = useState('')
   const [showDropdown, setShowDropdown] = useState(true)
   const [showResult, setShowResult] = useState(false)
+
+  var desc145 = 'Problem-solving, algorithmic design, and programming. Two lecture hours and four laboratory hours per week. Open to all majors. Carolina Core ARP.'
+  var review1 =  'This course was great for basic intro programming concepts and algorithms'
+  var review2 = 'Perfect course for an intro to Java. Homeworks arent bad but can be time consuming. Grades made up of . Definitely recommend this class!'
+  var review3 = 'Great class. Take JJ !!!'
 
   const onChange = (e) => {
     setShowDropdown(true)
@@ -43,8 +49,8 @@ function App() {
             <p className="searchText">Search Course</p>
             <input type="text" onChange={onChange} value={value} className="search-input" placeholder="Enter class (ex. CSCE 145)" />
             <button onClick={() => onSearch(value)}> Search </button>
+            
           </div>
-
           {showDropdown &&
           <div className="dropdown">
             {
@@ -59,10 +65,26 @@ function App() {
           {showResult &&
             <div className="result">
               <p className='courseCode'>{value}</p>
-              <p className='courseName'>{selectedTitle}</p>
-              <p className='recommend'>Recommend Course: 84%</p>
-              <p className='courseDescription'>Description</p>
-              <p className='courseDescription'>Reviews</p>
+                <p className='courseName'>{selectedTitle}</p>
+              <p className='courseDescription'><strong>Description: </strong>{desc145}<p>NOTE: This class uses Java.</p></p>
+              {value =='CSCE 145' &&
+                <div className='courseReviews'>  
+                <button className='addReview'>Add Review</button>
+                <p className='reviews'><strong>Reviews:</strong></p>
+                <hr></hr>
+                <div className = 'reviewList'>
+                  <p className='review1'><strong>User6289: </strong>{review1}</p>
+                    <p><strong>Professor:</strong> Unknown</p>
+                  <hr></hr>
+                  <p className='review2'><strong>Anonymous: </strong>{review2}</p>
+                    <p><strong>Professor:</strong> JJ Shepherd</p>
+                  <hr></hr>
+                  <p className='review3'><strong>Go_Cocks2435: </strong>{review3}</p>
+                    <p><strong>Professor:</strong> JJ Shepherd</p>
+                  <hr></hr>
+                </div>
+              </div>
+              } 
             </div>
           }
         </div>
